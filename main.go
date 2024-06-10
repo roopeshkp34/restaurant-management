5cb487543baf39203aa34308915186629abe8dc4
roopeshkp34 <roopeshkp34@gmail.com>
2024-06-10 22:43:11 +0530

    :tada: created base structure , food and user routes

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/roopeshkp34/restaurent-management/database"
+	"github.com/roopeshkp34/restaurent-management/middleware"
+	"github.com/roopeshkp34/restaurent-management/routes"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.User(middleware.Authentication)
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+	router.Run(":" + port)
+
+}
