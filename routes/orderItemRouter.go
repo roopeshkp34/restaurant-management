f563804ae026afb63d6ae4ef65d3c5361003459e
roopeshkp34 <roopeshkp34@gmail.com>
2024-06-13 20:33:06 +0530

    :construction: user, table, order, orderitems, note models completed

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -10,7 +10,7 @@ func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controllers.GetOrderItems())
 	incomingRoutes.GET("/orderItems/:orderItem_id", controllers.GetOrderItem())
 	incomingRoutes.POST("/orderItems", controllers.CreateOrderItem())
-	incomingRoutes.PATCH("/orderItems/:orderItem_id", controllers.UpdateOrderItems())
+	incomingRoutes.PATCH("/orderItems/:orderItem_id", controllers.UpdateOrderItem())
 	incomingRoutes.GET("/orderItems-order/:order_id", controllers.GetOrderItemsByOrder())
 
 }
