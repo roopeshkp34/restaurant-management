c519cf4acb7eea8d404aecb8d43f6cf2ce7d0551
roopeshkp34 <roopeshkp34@gmail.com>
2024-06-21 23:24:24 +0530

    :zap: completed items by order

--- controllers/orderItemControler.go
+++ controllers/orderItemControler.go
@@ -167,5 +167,52 @@ func UpdateOrderItem() gin.HandlerFunc {
 }
 
 func itemsByOrder(id string) (OrderItems []primitive.M, err error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+	matchStage := bson.D{{"$match", bson.D{{"order_id", id}}}}
+	lookUpStage := bson.D{{"$lookup", bson.D{{"from", "food"}, {"localField", "food_id"}, {"foreignField", "food_id"}, {"as", "food"}}}}
+	unwindStage := bson.D{{"$unwind", bson.D{{"path", "$food"}, {"preserveNullAndEmptyArrays", true}}}}
+
+	lookUpOrderStage := bson.D{{"$lookup", bson.D{{"from", "order"}, {"localField", "order_id"}, {"foreignField", "order_id"}, {"as", "order"}}}}
+	unwindOrderStage := bson.D{{"$unwind", bson.D{{"path", "$order"}, {"preserveNullAndEmptyArrays", true}}}}
+
+	lookupTableStage := bson.D{{"$lookup", bson.D{{"from", "table"}, {"localField", "order.Table_id"}, {"foreignField", "table_id"}, {"as", "table"}}}}
+	unwindTableStage := bson.D{{"$unwind", bson.D{{"path", "$table"}, {"preserveNullAndEmptyArrays", true}}}}
+
+	projectStage := bson.D{{"$project", bson.D{
+		{"id", 0},
+		{"amount", "$food.price"},
+		{"total_count", 1},
+		{"food_name", "$food.name"},
+		{"food_image", "$food.food_image"},
+		{"table_number", "$table.table_number"},
+		{"table_id", "$table.table_id"},
+		{"order_id", "$order.order_id"},
+		{"price", "$food.price"},
+		{"quantity", 1},
+	}}}
+
+	groupStage := bson.D{{"$group", bson.D{{"_id", bson.D{{"order_id", "$order_id"}, {"table_id", "$table_id"}, {"table_number", "$table_number"}}}, {"payment_due", bson.D{{"$sum", "$amount"}}}, {"total_count", bson.D{{"$sum", 1}}}, {"order_items", bson.D{{"$push", ""}}}}}}
+
+	projectStage2 := bson.D{
+		{"$project", bson.D{
+			{"_id", 0},
+			{"payment_due", 1},
+			{"total_count", 1},
+			{"table_number", "$_id.table-number"},
+			{"order_items", 1},
+		}}}
+
+	result, err := orderCollection.Aggregate(ctx, mongo.Pipeline{
+		matchStage, lookUpStage, unwindStage, lookUpOrderStage, unwindOrderStage, lookupTableStage, unwindTableStage, projectStage, groupStage, projectStage2,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	if err := result.All(ctx, &OrderItems); err != nil {
+		panic(err)
+	}
+	defer cancel()
+	return OrderItems, err
 }
